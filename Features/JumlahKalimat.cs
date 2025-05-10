7cacd7846d82099cad7b6af1e732f258e0f3be89
Timotiusadhi <148249433+Timotiusadhi@users.noreply.github.com>
2025-05-10 10:58:29 +0700

    Fitur rata-rata kata dan jumlah kalimat

---
+++ Features/JumlahKalimat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SimpleWordCounter.Features
+{
+    public class JumlahKalimat
+    {
+        private enum State { InText, EndPunctuation }
+
+        public static int CountSentences(string text)
+        {
+            State currentState = State.InText;
+            int sentenceCount = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+
+                switch (currentState)
+                {
+                    case State.InText:
+                        if (c == '.' || c == '!' || c == '?')
+                        {
+                            sentenceCount++;
+                            currentState = State.EndPunctuation;
+                        }
+                        break;
+
+                    case State.EndPunctuation:
+                        if (!char.IsWhiteSpace(c) && char.IsLetterOrDigit(c))
+                        {
+                            currentState = State.InText;
+                        }
+                        break;
+                }
+            }
+
+            return sentenceCount;
+        }
+    }
+}
