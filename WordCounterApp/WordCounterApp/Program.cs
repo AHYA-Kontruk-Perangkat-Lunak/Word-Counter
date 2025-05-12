ec2652a9d985afa58a7a433f01931adc1995c3d0
armdntoo <armadantoyafet@gmail.com>
2025-05-12 13:22:18 +0700

    Fitur Word Frequency

---
+++ WordCounterApp/WordCounterApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;  
+using System.Collections.Generic;
+using System.Diagnostics;  
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Masukkan teks yang ingin dihitung frekuensi katanya:");
+
+        string inputText = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(inputText))
+        {
+            Console.WriteLine("Teks tidak boleh kosong.");
+            return; 
+        }
+
+        var stopwatch = Stopwatch.StartNew();
+
+        var wordFrequency = TextUtilities.GetWordFrequency(inputText, 3);  
+
+        stopwatch.Stop();
+
+        Console.WriteLine($"\nWaktu eksekusi: {stopwatch.ElapsedMilliseconds} ms");
+
+        var sortedWordFrequency = wordFrequency.OrderByDescending(kvp => kvp.Value);
+
+        Console.WriteLine("\nKata yang Paling Sering Muncul:");
+        foreach (var entry in sortedWordFrequency)
+        {
+            Console.WriteLine($"Kata: {entry.Key}, Frekuensi: {entry.Value}");
+        }
+
+        Console.ReadLine();
+    }
+}
